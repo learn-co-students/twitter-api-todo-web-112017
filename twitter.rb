fa2be1f294462a6a0bd13d791d5d9568ae40ad59
ddbrennan <ddbren@gmail.com>
2017-11-25 13:57:46 -0500

    add twitter api functionality

--- twitter.rb
+++ twitter.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 require 'yaml'
+require 'pry'
 
 class TwitterApi
   attr_reader :client
@@ -16,30 +17,30 @@ class TwitterApi
 
 
   def most_recent_friend
-    #find the twitter gem method that retrieves a user's friends and grab the most recent friend
+    client.friends.first
   end
 
   def find_user_for(username)
-    #find the twitter gem method that returns the correct user, given a username
+    client.user(username)
   end
 
   def find_followers_for(username)
-    #find the twitter gem method that returns the follows of a given user
+    client.followers(username).attrs[:users][0..9]
   end
 
   def homepage_timeline
-    #find the twitter gem method that retreives the tweets from a user's timeline.
+    client.home_timeline
   end
-  
+
 end
 
-#Bonus: 
+#Bonus:
 
 # uncomment out the following and read the bonus instructions.
 # remember to comment out the WebMock line of your spec_helper, as the instructions dictate.
 
-# tweet_client = TwitterApi.new
-# puts tweet_client.most_recent_friend
-# puts tweet_client.find_user_for("USERNAME HERE")
-# puts tweet_client.find_followers_for("USERNAME HERE")
-# puts tweet_client.homepage_timeline
+tweet_client = TwitterApi.new
+puts tweet_client.most_recent_friend.name
+puts tweet_client.find_user_for("formal_jorts").name
+tweet_client.find_followers_for("formal_jorts").each {|user| puts user[:name]}
+tweet_client.homepage_timeline.each {|tweet| puts tweet.text}
